212d5709d952522f3ab615f354f1edf2a87a75fc
FelixZett <FelixZett@users.noreply.github.com>
2020-11-30 21:04:30 +0100

    Update test framework

--- src/test/kotlin/com/felixzett/mmkqe/repository/TestEntityRepositoryTest.kt
+++ src/test/kotlin/com/felixzett/mmkqe/repository/TestEntityRepositoryTest.kt
@@ -1,21 +1,18 @@
 package com.felixzett.mmkqe.repository
 
 import com.felixzett.mmkqe.entity.TestEntity
-import junit.framework.TestCase.assertEquals
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class TestEntityRepositoryTest {
     @Autowired
     private lateinit var testEntityRepository: TestEntityRepository
 
-    @Before
+    @BeforeEach
     fun setup() {
         testEntityRepository.save(TestEntity("test", 8.0))
     }
